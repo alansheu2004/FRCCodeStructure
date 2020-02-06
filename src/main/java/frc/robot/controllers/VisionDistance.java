555f043417f20d3ef79ebaa19315648a15504c42
AlexanderFairchild <>
2020-02-05 16:03:34 -0800

    Update VisionDistance.java

--- src/main/java/frc/robot/controllers/VisionDistance.java
+++ src/main/java/frc/robot/controllers/VisionDistance.java
@@ -1,14 +1,6 @@
 package frc.robot.controllers;
-
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-
-import java.sql.Time;
-
-import frc.robot.util.AdditionalMath;
 import frc.robot.util.Context;
-import frc.robot.util.PID;
+
 public class VisionDistance
 {
     double targetHeight = 2.1336; //7 feet in meters
